9316e9a568ba8c57e7781513f1d0b4f6aed352f3
David Persson <davidpersson@gmx.de>
2015-01-11 12:34:56 +0100

    Fix typos in documentation.

--- src/Client.php
+++ src/Client.php
@@ -14,7 +14,7 @@ use RuntimeException;
 
 /**
  * An interface to the beanstalk queue service. Implements the beanstalk
- * protocol spec 1.9. Where appropriate the documentation from the protcol
+ * protocol spec 1.9. Where appropriate the documentation from the protocol
  * has been added to the docblocks in this class.
  *
  * @link https://github.com/kr/beanstalkd/blob/master/doc/protocol.txt
